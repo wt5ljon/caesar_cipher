49864b3bb52664c0250df12c9179bde46abc65d7
Jon Olson <jonolson64@outlook.com>
2016-12-20 16:42:36 -0500

    clean up and comment

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,24 +1,24 @@
-def caesar_cipher(str, num)
-	secret = ""
-	puts str
-	len = str.length - 1
-	(0..len).each do |i|
-		char = str.getbyte(i).to_i
-		if str[i] >= "A" and str[i] <= "Z"
-			char = char + num
+# implements the Caesar cipher function
+#
+def caesar_cipher(text, num)
+	(0..text.length-1).each do |i|
+		# get the ascii value of each character
+		char = text.getbyte(i).to_i
+		if text[i] >= "A" and text[i] <= "Z"
+			char = char + (num % 26)
 			if char > 90
 				char = char - 26
 			end
-		elsif str[i] >= "a" and str[i] <= "z"
-			char = char + num
+		elsif text[i] >= "a" and text[i] <= "z"
+			char = char + (num % 26)
 			if char > 122 
 				char = char - 26
 			end
 		end
-		str.setbyte(i, char)
+		# replace the character
+		text.setbyte(i, char)
 	end
-	puts str
+	return text
 end
 
-caesar_cipher("What a string!", 5) 
-
+puts caesar_cipher("What a string!", 13)
